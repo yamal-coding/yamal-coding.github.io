3e8c49fc66a1a61a93f94c99809af7e1620b7b68
yamal <yamal.coding@gmail.com>
2017-08-25 18:14:13 +0200

    Optimized Menu

--- js/jqmenu.js
+++ js/jqmenu.js
@@ -2,40 +2,12 @@ var local = false;
 
 $(document).ready(function() {
 	$("#cajaMenu").load(((local) ? 'http://localhost:8000' : 'https://yamal-coding.github.io') + '/files/menucontent.html', function(){
-		$("#aboutmeButton").hover(function(){
-			$("#imagenAboutme").css("top", $("#aboutmeButton").position().top + 50)
-						.css("left", $("#aboutmeButton").position().left)
+		$('.boton').hover(function(){
+			$('#' + $(this).attr('imagen')).css("top", $(this).position().top + 50)
+						.css("left", $(this).position().left)
 						.show("500");
 		}, function(){
-			$("#imagenAboutme").hide("500");
-		});
-		$("#githubButton").hover(function(){
-			$("#imagenGit").css("top", $("#githubButton").position().top + 50)
-						.css("left", $("#githubButton").position().left)
-						.show("500");
-		}, function(){
-			$("#imagenGit").hide("500");
-		});
-		$("#artButton").hover(function(){
-			$("#imagenArt").css("top", $("#artButton").position().top + 50)
-						.css("left", $("#artButton").position().left)
-						.show("500");
-		}, function(){
-			$("#imagenArt").hide("500");
-		});
-		$("#linkedinButton").hover(function(){
-			$("#imagenLink").css("top", $("#linkedinButton").position().top + 50)
-						.css("left", $("#linkedinButton").position().left)
-						.show("500");
-		}, function(){
-			$("#imagenLink").hide("500");
-		});
-		$("#projectsButton").hover(function(){
-			$("#imagenProjects").css("top", $("#projectsButton").position().top + 50)
-						.css("left", $("#projectsButton").position().left)
-						.show("500");
-		}, function(){
-			$("#imagenProjects").hide("500");
+			$('#' + $(this).attr('imagen')).hide("500");
 		});
 	});
 });
